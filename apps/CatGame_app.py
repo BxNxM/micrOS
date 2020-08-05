30db934158375cfb4312e866c86f73d3a5b91785
BNM <ban.marcell@gmail.com>
2020-08-05 19:34:54 +0200

    CatGame_app endless servo update

--- apps/CatGame_app.py
+++ apps/CatGame_app.py
@@ -2,48 +2,52 @@
 
 import os
 import sys
+import atexit
 from random import randint
 MYPATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(MYPATH, '../tools'))
 import socketClient
 
 DEVICE = 'ImpiGame'
-BASE_CMD = ['--dev', DEVICE ]
+BASE_CMD = ['--dev', DEVICE]
 CMD_PIPE_SEP = '<a>'
+SERVO_CENTER_VAL = 77
 
-def app(iteration=60):
+
+def app(iteration=30):
     global DEVICE, BASE_CMD, CMD_PIPE_SEP
     for _ in range(iteration):
         piped_commands = []
 
-        args = BASE_CMD + ['servo', 'Servo(40)']
+        args = BASE_CMD + ['servo', 'Servo({})'.format(SERVO_CENTER_VAL)]
         print("CMD: {}".format(args))
         args.append(CMD_PIPE_SEP)
         piped_commands += args
 
         for _ in range(randint(1, 6)):
-            duty = randint(50, 110)
+            duty = randint(55, 100)
             args = ['servo', 'Servo({duty})'.format(duty=duty)]
             print("\tCMD: {}".format(args))
             args.append(CMD_PIPE_SEP)
             piped_commands += args
 
-        args = ['servo', 'Servo(120)']
-        print("\tCMD: {}".format(args))
-        args.append(CMD_PIPE_SEP)
-        piped_commands += args
-
-        args = ['servo', 'Servo(80)']
+        args += ['servo', 'Servo({})'.format(SERVO_CENTER_VAL)]
         print("CMD: {}".format(args))
-        args.append(CMD_PIPE_SEP)
         piped_commands += args
 
         print("CMD PIPE: {}".format(piped_commands))
         socketClient.run(piped_commands)
 
-    args = BASE_CMD + ['servo', 'Servo_deinit']
+    deinit_servo()
+
+
+def deinit_servo():
+    args = [BASE_CMD, ['servo', 'Servo({})'.format(SERVO_CENTER_VAL)], ['servo', 'Servo_deinit']]
+    print("DEINIT SERVO, SET TO {} and DEINIT".format(SERVO_CENTER_VAL))
     socketClient.run(args)
 
+
+atexit.register(deinit_servo)
 
 if __name__ == "__main__":
     app()

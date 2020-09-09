3e2209e93f32b6dabb2123c028ff1c2415dc1882
BNM <ban.marcell@gmail.com>
2020-09-09 15:42:14 +0200

    Scheduler logic proto on timirq #2

--- MicrOS/Scheduler.py
+++ MicrOS/Scheduler.py
@@ -1,15 +1,15 @@
 from time import sleep
-IRQSEQ = 1000
+IRQSEQ = 5000
 
 # SYSTEM TIME FORMAT:    Y, M, D, H, M, S, WD, YD
 # SCHEDULER TIME FORMAT: WD, H, M, S
 # 1:18:2:1
 # *:*:*:*
-scheduler_input = [('1:18:20:5', "MONDAY 18:20 EVENT"),
-                   ('3:20:1:5', "WEDNESDAY 20:1 EVENT"),
+scheduler_input = [('1:18:20:5', "MONDAY EVENT"),
+                   ('3:20:1:5', "WEDNESDAY EVENT"),
                    ('*:8:15:5', "LAMP ON EVERYDAY"),
-                   ('*:10:0:5', "LAMP OFF EVERYDAY"),
-                   ('5:20:0:*', "EVERY SEC ON FRIDAY 20:0")]
+                   ('*:10:1:5', "LAMP OFF EVERYDAY"),
+                   ('5:20:0:*', "EVERY SEC ON FRIDAY")]
 
 #############################
 #     SYSTEM TEST MODULES   #
@@ -20,7 +20,7 @@ def systime(sec):
     h = int(sec / 60 / 60) % 24
     m = int(sec / 60 % 60)
     s = sec % 60
-    wd = int(sec / 60 / 60 / 24) % 7
+    wd = 1 + int(sec / 60 / 60 / 24) % 7
     day = int(sec/60/60/24) % 30
     return 2020, 9, day, h, m, s, wd, 0
 
@@ -41,36 +41,58 @@ GEN = system_time_generator()
 #############################
 
 
+def __convert_sec_to_time(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return hour, minutes, seconds
+
+
 def __scheduler_trigger(scheduler_fragment, sec_tolerance=2):
+    sec_tolerance *= 2
     # Format: WD, H, M, S
     check_time = tuple(int(t.strip()) if t.strip() != '*' else t.strip() for t in scheduler_fragment[0].split(':'))
     # Format: Y, M, D, H, M, S, WD, YD
     time_now = GEN.__next__()       # TODO: replace with system time
     check_time_now = (time_now[-2], time_now[-5], time_now[-4], time_now[-3])
-    # (Mmin, Smin), (Mmax, Smax) time frame correction
-    tolerance_max_m_s = (int(((check_time_now[-2] * 60 + check_time_now[-1]) + sec_tolerance) / 60), int(check_time_now[-1] + sec_tolerance) % 60)
-    tolerance_min_m_s = (int(((check_time_now[-2] * 60 + check_time_now[-1]) - sec_tolerance) / 60), int(check_time_now[-1] - sec_tolerance) % 60)
-    #print("|{}<->{}|{}|\n".format(tolerance_min_m_s, tolerance_max_m_s, check_time_now))
-    print(time_now, end='\r')               # TODO: remove
-    sleep(0.0001)                           # TODO: REMOVE
+    check_time_now_sec = (check_time_now[1] * 3600) + (check_time_now[2] * 60) + check_time_now[3]
+    # Time frame +/- corrections
+    tolerance_min = __convert_sec_to_time(check_time_now_sec - sec_tolerance)
+    tolerance_max = __convert_sec_to_time(check_time_now_sec + sec_tolerance)
+    print("[{}]\tmin{}<->max{}\tdelta: +/- {}".format(check_time_now,
+                                                      tolerance_min,
+                                                      tolerance_max,
+                                                      sec_tolerance), end='\r')
+    sleep(0.0001) # TODO: REMOVE
     # Check WD - WEEK DAY
     if check_time[0] == '*' or check_time[0] == check_time_now[0]:
         # Check H - HOUR
-        if check_time[1] == '*' or check_time[1] == check_time_now[1]:
+        if check_time[1] == '*' or tolerance_min[0] <= check_time[1] <= tolerance_max[0]:
             # Check M - MINUTE
-            if check_time[2] == '*' or tolerance_min_m_s[0] <= check_time[2] <= tolerance_max_m_s[0]:
+            if check_time[2] == '*' or tolerance_min[1] <= check_time[2] <= tolerance_max[1]:
                 # Check S - SECOND
-                if check_time[3] == '*' or tolerance_min_m_s[1] <= check_time[3] <= tolerance_max_m_s[1]:
+                if check_time[3] == '*' or tolerance_min[2] <= check_time[3] <= tolerance_max[2]:
                     # Execute load module TODO
                     print("[EXEC][{} -- {}] EXECUTE LM: {}".format(check_time_now,
                                                                    scheduler_fragment[0],
                                                                    scheduler_fragment[1]))
+                '''
+                else:
+                    print("[SKIP EXEC][{} -- {}] EXECUTE LM: {} [{}-{}]".format(check_time_now,
+                                                                   scheduler_fragment[0],
+                                                                   scheduler_fragment[1],
+                                                                   tolerance_min,
+                                                                   tolerance_max))
+                '''
 
 
 def scheduler():
     for cron in scheduler_input:
-        __scheduler_trigger(cron)
-
+        #__scheduler_trigger(cron)
+        __scheduler_trigger(cron, sec_tolerance=int(IRQSEQ/1000))
+        
 
 if __name__ == "__main__":
     #scheduler()

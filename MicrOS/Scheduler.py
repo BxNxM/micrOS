301e834b32209eae3bbda4987c0c8a2f20d1d833
BNM <ban.marcell@gmail.com>
2020-09-11 14:19:57 +0200

    Scheduler core verified, TODO: connect to micrOS

--- MicrOS/Scheduler.py
+++ MicrOS/Scheduler.py
@@ -1,21 +1,17 @@
-from time import sleep
-from random import randint
-IRQSEQ = 2000
+# DUMMY IRQ VALUE
+IRQSEQ = 1000
 
+'''
 # SYSTEM TIME FORMAT:    Y, M, D, H, M, S, WD, YD
 # SCHEDULER TIME FORMAT: WD, H, M, S
-# 1:18:2:1
-# *:*:*:*
-scheduler_input = [('1:10:20:0', "[1] MONDAY EVENT"),
-                   ('2:12:25:0', "[2] TUESDAY EVENT"),
-                   ('3:15:30:0', "[3] WEDNESDAY EVENT"),
-                   ('4:18:35:0', "[4] THURSDAY EVENT"),
-                   ('5:20:40:0', "[5] FRIDAY EVENT"),
-                   ('6:21:40:0', "[6] SATURDAY EVENT"),
-                   ('7:23:20:0', "[7] END OF WEEEEEEKKKK"),
-                   ('*:8:15:0', "[0*1] LAMP ON EVERYDAY"),
-                   ('*:10:1:0', "[0*2] LAMP OFF EVERYDAY")]
-LAST_CRON_TASK = ["", 1, 1]
+WD: 1-7
+H: 0-23
+M: 0-59
+S: 0-59
+* - means in every place - every time
+'''
+
+LAST_CRON_TASK = ["", ""]
 
 #############################
 #     SYSTEM TEST MODULES   #
@@ -32,13 +28,14 @@ def systime(sec):
 
 
 def system_time_generator(max=100000000):
+    from random import randint
     generator = (systime(sec) for sec in range(0, max, int(IRQSEQ/1000)+randint(-2, 2)))
     return generator
 
 
-def dummyirq_sec():
+def dummyirq_sec(raw_cron_input):
     while True:
-        scheduler()
+        scheduler(raw_cron_input)
 
 
 GEN = system_time_generator()
@@ -57,35 +54,13 @@ def __convert_sec_to_time(seconds):
     return hour, minutes, seconds
 
 
-def dimensioning_metica():
-    """Dump dimensioning data for timing feedback"""
-    print("------ [STAT] {} relevant/all triggers: {}% [{}][{}]".format(LAST_CRON_TASK[0],
-                                                                        int((LAST_CRON_TASK[1] / (
-                                                                             LAST_CRON_TASK[2] + LAST_CRON_TASK[
-                                                                             1])) * 100),
-                                                                        LAST_CRON_TASK[1],
-                                                                        LAST_CRON_TASK[2]))
-
-
-def dimensioning():
-    """Cron controller feedback value - for runtime sample time control"""
-    if LAST_CRON_TASK[1] >= 20 or LAST_CRON_TASK[2] >= 20:
-        normalize = LAST_CRON_TASK[1] if LAST_CRON_TASK[1] < LAST_CRON_TASK[2] else LAST_CRON_TASK[2]
-        LAST_CRON_TASK[1] -= normalize
-        LAST_CRON_TASK[2] -= normalize
-    return 10 * (1 - LAST_CRON_TASK[1] / (LAST_CRON_TASK[2] + LAST_CRON_TASK[1]))
-
-
-def __scheduler_trigger(scheduler_fragment, sec_tolerance=2):
+def __scheduler_trigger(time_now, scheduler_fragment, sec_tolerance=2):
     """
     SchedulerCore logic
+    check format: WD, H, M, S
     """
-    sec_tolerance += int(dimensioning() * sec_tolerance)  # time edge correction
-    # Format: WD, H, M, S
     check_time = tuple(int(t.strip()) if t.strip() != '*' else t.strip() for t in scheduler_fragment[0].split(':'))
     task_id = "{}{}{}{}{}".format(check_time[0], check_time[1], check_time[2], check_time[3], scheduler_fragment[1].replace(' ', ''))
-    # Format: Y, M, D, H, M, S, WD, YD
-    time_now = GEN.__next__()       # TODO: replace with system time
     check_time_now = (time_now[-2], time_now[-5], time_now[-4], time_now[-3])
     check_time_now_sec = (check_time_now[1] * 3600) + (check_time_now[2] * 60) + check_time_now[3]
     # Time frame +/- corrections
@@ -95,7 +70,6 @@ def __scheduler_trigger(scheduler_fragment, sec_tolerance=2):
                                                       tolerance_min,
                                                       tolerance_max,
                                                       sec_tolerance), end='\r')
-    sleep(0.0001) # TODO: REMOVE
     # Check WD - WEEK DAY
     if check_time[0] == '*' or check_time[0] == check_time_now[0]:
         # Check H - HOUR
@@ -107,40 +81,58 @@ def __scheduler_trigger(scheduler_fragment, sec_tolerance=2):
                 if tolerance_min[2] > tolerance_max[2]:
                     sec_range = tuple(list(range(tolerance_min[2], 60)) + list(range(0, tolerance_max[2])))
                 if check_time[3] == '*' or check_time[3] in sec_range:
-                    # In timerange call metric (sample)
-                    LAST_CRON_TASK[1] += 1
                     # FILTER TO AVOID REDUNDANT EXECUTION
-                    if check_time[3] == '*' or task_id not in LAST_CRON_TASK[0]:
-                        # Execute load module TODO
+                    if check_time[3] == '*' or task_id not in LAST_CRON_TASK:
+                        # TODO: replace with InterpreterCore
                         print("[EXEC]NOW[{}]  {} <-> {}  CONF[{}] EXECUTE LM: {}".format(check_time_now,
                                                                                    tolerance_min,
                                                                                    tolerance_max,
                                                                                    scheduler_fragment[0],
                                                                                    scheduler_fragment[1]))
-                        # Store last executed task_id
-                        LAST_CRON_TASK[0] = task_id
-                        dimensioning_metica()
-                else:
-                    # Out timerange call metric (sample)
-                    LAST_CRON_TASK[2] += 1
-                    """
-                    print("----------[SKIPEXEC]NOW[{}]  {} <-> {}  CONF[{}] EXECUTE LM: {}".format(check_time_now,
-                                                                                   tolerance_min,
-                                                                                   tolerance_max,
-                                                                                   scheduler_fragment[0],
-                                                                                   scheduler_fragment[1]))
-                    """
+                        # EXECUTED CACHE
+                        if check_time[3] != '*' and check_time[2] != '*':
+                            # SAVE WHEN S and M not *
+                            LAST_CRON_TASK[1] = task_id
+                        elif check_time[2] != '*':
+                            # SAVE WHEN S not *
+                            LAST_CRON_TASK[0] = task_id
+                        return True
+    return False
 
 
-def scheduler():
+def deserialize_raw_input(raw_cron_input):
+    datastruct = []
+    try:
+        datastruct = [tuple(cron.split('!')) for cron in raw_cron_input.split(';')]
+    except Exception as e:
+        print("deserialize_raw_input: input syntax error: {}".format(e))
+    return datastruct
+
+
+def scheduler(raw_cron_input):
+    """RAW INPUT SYNTAX:
+        '{cron time}!COMD;{cron time2}!COMD2;...'
+    ! - execute
+    ; - cron task separator
+    """
+    scheduler_input = deserialize_raw_input(raw_cron_input)
+    return_state = False
+    # Format: Y, M, D, H, M, S, WD, YD
+    time_now = GEN.__next__()       # TODO: replace with system time
     for cron in scheduler_input:
-        #__scheduler_trigger(cron)
-        __scheduler_trigger(cron, sec_tolerance=int(IRQSEQ/1000))
+        state = __scheduler_trigger(time_now, cron, sec_tolerance=int(IRQSEQ/1000))
+        return_state |= state
+    return return_state
 
 
 if __name__ == "__main__":
-    #scheduler()
-    dummyirq_sec()
+    raw_cron_input = '*:10:23:*![0] EVERY SEC;1:10:20:59!\t[1] MONDAY EVENT;\
+2:12:25:1!\t[2] TUESDAY EVENT;3:15:30:30!\t[3] WEDNESDAY EVENT;4:18:35:23!\t[4] THURSDAY EVENT;\
+5:20:40:56!\t[5] FRIDAY EVENT;6:21:40:0!\t[6] SATURDAY EVENT;7:23:20:0![7] END OF WEEEEEEKKKK;\
+*:8:15:0!\t\t[0*1] LAMP ON EVERYDAY;*:10:1:0!\t\t[0*2] LAMP OFF EVERYDAY'
+
+    #scheduler(raw_cron_input)
+    dummyirq_sec(raw_cron_input)
 
 
 

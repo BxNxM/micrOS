04980480253f66e307b9c805dd6399c9f1023f5b
BNM <ban.marcell@gmail.com>
2020-10-19 19:00:03 +0200

    micrOSdashboard GUI structure finalization

--- tools/micrOSdashboard.py
+++ tools/micrOSdashboard.py
@@ -22,26 +22,55 @@ class App(QWidget):
         self.pbar = None
         self.pbar_status = 0
         self.dropdown_objects_list = {}
-        self.console = MyConsole(self)
+        self.data_set = {'force': False}
+        self.console = None
 
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setFixedWidth(self.width)
+        self.setFixedHeight(self.height)
         self.setStyleSheet("background-color: grey")
-        self.progressbar()
+        self.venv_indicator()
+
+    def create_console(self):
+        dropdown_label = QLabel(self)
+        dropdown_label.setText("Console".upper())
+        dropdown_label.setStyleSheet("background-color : darkGray;")
+        dropdown_label.setGeometry(250, 117, 420, 15)
+        self.console = MyConsole(self)
+
+    def venv_indicator(self):
+        state = False
+        if hasattr(sys, 'real_prefix'):
+            state = True
+
+        if state:
+            label = QLabel(' [devEnv] virtualenv active', self)
+            label.setGeometry(20, 5, self.width-50, 20)
+            label.setStyleSheet("background-color : green;")
+        else:
+            label = QLabel(' [devEnv] virtualenv inactive', self)
+            label.setGeometry(20, 5, self.width-50, 20)
+            label.setStyleSheet("background-color : yellow;")
 
     def buttons(self):
-        height = 40
+        height = 35
         width = 200
-        yoffset = 10
-        buttons = {'Deploy over USB': ['This is an example button',
-                                       20, 100, width, height, self.on_click_usb_deploy, 'darkCyan'],
-                   'OTA-OverTheAir update': ['This is an example button',
-                                       20, 100+height+yoffset, width, height, self.on_click_ota_update, 'darkCyan'],
-                   'Update over USB': ['This is an example button',
-                                       20, 100+(height+yoffset)*2, width, height, self.on_click_usb_update, 'darkCyan']}
+        yoffset = 3
+        buttons = {'Deploy (USB)': ['Install "empty" device, deploy micropython and micrOS Framework',
+                                       20, 115, width, height, self.__on_click_usb_deploy, 'darkCyan'],
+                   'Update (OTA)': ['OTA - Over The Air (wifi) update, upload micrOS resources over webrepl',
+                                  20, 115 + height + yoffset, width, height, self.__on_click_ota_update, 'darkCyan'],
+                   'Update (USB)': ['Update micrOS (with redeploy micropython) over USB',
+                                       20, 115 + (height+yoffset) * 2, width, height, self.__on_click_usb_update, 'darkCyan'],
+                   'Search device': ['Search online micrOS devices on local network (wifi)',
+                                      20, 115 + (height + yoffset) * 3, width, height, self.__on_click_serach_devices, 'darkCyan'],
+                   'Simulator': ['Start micrOS on host with micropython dummy (module) interfaces',
+                                      20, 115 + (height + yoffset) * 4, width, height, self.__on_click_simulator, 'darkCyan']
+                   }
 
         for key, data_struct in buttons.items():
             tool_tip = data_struct[0]
@@ -69,26 +98,38 @@ class App(QWidget):
             button.clicked.connect(event_cbf)
 
     @pyqtSlot()
-    def on_click_usb_deploy(self):
-        print('on_click_usb_deploy')
-        self.console.append_output('on_click_usb_deploy')
+    def __on_click_usb_deploy(self):
+        print('__on_click_usb_deploy')
+        self.console.append_output('__on_click_usb_deploy')
         self.progressbar_update()
 
     @pyqtSlot()
-    def on_click_ota_update(self):
-        print('on_click_ota_update')
-        self.console.append_output('on_click_ota_update')
+    def __on_click_ota_update(self):
+        print('__on_click_ota_update')
+        self.console.append_output('__on_click_ota_update')
         self.progressbar_update()
 
     @pyqtSlot()
-    def on_click_usb_update(self):
-        print('on_click_usb_update')
-        self.console.append_output('on_click_usb_update')
+    def __on_click_usb_update(self):
+        print('__on_click_usb_update')
+        self.console.append_output('__on_click_usb_update')
+        self.progressbar_update()
+
+    @pyqtSlot()
+    def __on_click_serach_devices(self):
+        print('__on_click_serach_devices')
+        self.console.append_output('__on_click_serach_devices')
+        self.progressbar_update()
+
+    @pyqtSlot()
+    def __on_click_simulator(self):
+        print('__on_click_simulator')
+        self.console.append_output('__on_click_simulator')
         self.progressbar_update()
 
     def draw_logo(self):
         label = QLabel(self)
-        label.setGeometry(20, 10, 80, 80)
+        label.setGeometry(20, 30, 80, 80)
         label.setScaledContents(True)
         pixmap = QPixmap('/Users/bnm/Documents/NodeMcu/MicrOs/media/logo_mini.png')
         label.setPixmap(pixmap)
@@ -98,7 +139,7 @@ class App(QWidget):
         self.pbar = QProgressBar(self)
 
         # setting its geometry
-        self.pbar.setGeometry(10, self.height-40, self.width-20, 25)
+        self.pbar.setGeometry(20, self.height-40, self.width-70, 30)
 
     def progressbar_update(self, value=None, reset=False):
         if reset:
@@ -110,18 +151,26 @@ class App(QWidget):
         print(self.pbar_status)
 
     def draw(self):
+        self.progressbar()
+        self.create_console()
+        self.draw_logo()
+        self.buttons()
+        self.dropdown_board()
+        self.dropdown_micropythonbin()
+        self.dropdown_device()
+        self.force_mode_radiobutton()
         self.show()
 
     def dropdown_board(self):
         dropdown_label = QLabel(self)
         dropdown_label.setText("Select board".upper())
-        dropdown_label.setGeometry(250, 84, 170, 30)
+        dropdown_label.setGeometry(120, 30, 170, 30)
 
         # creating a combo box widget
         combo_box = QComboBox(self)
 
         # setting geometry of combo box
-        combo_box.setGeometry(250, 110, 170, 30)
+        combo_box.setGeometry(120, 60, 170, 30)
 
         # geek list
         geek_list = ["esp8266", "esp32"]
@@ -150,18 +199,18 @@ class App(QWidget):
         view.setHidden(False)
 
         self.dropdown_objects_list['board'] = combo_box
-        combo_box.activated.connect(self.get_dropdown_values)
+        combo_box.activated.connect(self.get_widget_values)
 
     def dropdown_micropythonbin(self):
         dropdown_label = QLabel(self)
         dropdown_label.setText("Select micropython".upper())
-        dropdown_label.setGeometry(450, 84, 170, 30)
+        dropdown_label.setGeometry(310, 30, 170, 30)
 
         # creating a combo box widget
         combo_box = QComboBox(self)
 
         # setting geometry of combo box
-        combo_box.setGeometry(450, 110, 170, 30)
+        combo_box.setGeometry(310, 60, 170, 30)
 
         # geek list
         geek_list = ["micropython_1.11.bin", "micropython_1.12.bin"]
@@ -190,18 +239,18 @@ class App(QWidget):
         view.setHidden(False)
 
         self.dropdown_objects_list['micropython'] = combo_box
-        combo_box.activated.connect(self.get_dropdown_values)
+        combo_box.activated.connect(self.get_widget_values)
 
     def dropdown_device(self):
         dropdown_label = QLabel(self)
         dropdown_label.setText("Select device".upper())
-        dropdown_label.setGeometry(250, 140, 170, 20)
+        dropdown_label.setGeometry(500, 35, 170, 20)
 
         # creating a combo box widget
         combo_box = QComboBox(self)
 
         # setting geometry of combo box
-        combo_box.setGeometry(250, 160, 170, 30)
+        combo_box.setGeometry(500, 60, 170, 30)
 
         # geek list
         geek_list = ["BedLamp", "airquality", "mazsola"]
@@ -230,31 +279,51 @@ class App(QWidget):
         view.setHidden(False)
 
         self.dropdown_objects_list['device'] = combo_box
-        combo_box.activated.connect(self.get_dropdown_values)
+        combo_box.activated.connect(self.get_widget_values)
 
-    def get_dropdown_values(self, index):
-        board = "BOARD: {}".format(self.dropdown_objects_list['board'].itemText(index))
-        micropython = "MICROPYTHON: {}".format(self.dropdown_objects_list['micropython'].itemText(index))
-        device = "DEVICE: {}".format(self.dropdown_objects_list['device'].itemText(index))
-        self.console.append_output(board)
-        self.console.append_output(micropython)
-        self.console.append_output(device)
-        print(board)
-        print(micropython)
-        print(device)
+    def get_widget_values(self, index):
+        self.data_set['board'] = self.dropdown_objects_list['board'].itemText(index)
+        self.data_set['micropython'] = self.dropdown_objects_list['micropython'].itemText(index)
+        self.data_set['device'] = self.dropdown_objects_list['device'].itemText(index)
+        self.__show_gui_state_on_console()
+        return self.data_set
+
+    def __show_gui_state_on_console(self):
+        self.console.append_output("micrOS GUI Info")
+        for key, value in self.data_set.items():
+            self.console.append_output("  {}: {}".format(key, value))
+
+    def force_mode_radiobutton(self):
+        #label = QLabel('Force mode for version override')
+        rbtn1 = QRadioButton('Force mode', self)
+        rbtn1.move(20, self.height-60)
+        rbtn1.toggled.connect(self.__on_click_force_mode)
+
+        #layout = QVBoxLayout()
+        #layout.setGeometry(0, 0, 100, 20)
+        #layout.addWidget(label)
+        #layout.addWidget(rbtn1)
+        #self.setLayout(layout)
+
+    def __on_click_force_mode(self):
+        radioBtn = self.sender()
+        if radioBtn.isChecked():
+            self.data_set['force'] = True
+        else:
+            self.data_set['force'] = False
+        self.__show_gui_state_on_console()
 
 
 class MyConsole(QPlainTextEdit):
-    #https://gist.github.com/blubberdiblub/007bb92991d01ad29877931f75260b39
+    console = None
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-
         self.setReadOnly(True)
         self.setMaximumBlockCount(20000)  # limit console to 20000 lines
-
         self._cursor_output = self.textCursor()
-        self.setGeometry(250, 210, 400, 150)
+        self.setGeometry(250, 132, 420, 175)
+        MyConsole.console = self
 
     @pyqtSlot(str)
     def append_output(self, text, end='\n'):
@@ -272,13 +341,6 @@ class MyConsole(QPlainTextEdit):
 def main():
     app = QApplication(sys.argv)
     ex = App()
-
-    ex.draw_logo()
-    ex.buttons()
-    ex.dropdown_board()
-    ex.dropdown_micropythonbin()
-    ex.dropdown_device()
-
     ex.draw()
     sys.exit(app.exec_())
 

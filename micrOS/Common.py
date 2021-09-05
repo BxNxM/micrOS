d1f9c4c59739c8c94b65e76415b0a48a65981e49
BNM <ban.marcell@gmail.com>
2021-09-05 21:48:44 +0200

    [MICROS] New common module for transition generator logic + neopixel transition and set_transition functions - version: 1.2.1-5

--- micrOS/Common.py
+++ micrOS/Common.py
@@ -20,3 +20,20 @@ def socket_stream(func):
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs, msgobj=SocketServer().reply_message)
     return wrapper
+
+
+def transition(from_val, to_val, step_ms, interval_sec):
+    """
+    Generator for color transitions:
+    :param from_val: from value - start from
+    :param to_val: to value - target value
+    :param step_ms: step to reach to_val - timirq_seq
+    :param interval_sec: full interval
+    """
+    from_val = from_val
+    step_cnt = round((interval_sec*1000)/step_ms)
+    delta = abs((from_val-to_val)/step_cnt)
+    direc = -1 if from_val > to_val else 1
+
+    for cnt in range(0, step_cnt+1):
+        yield round(from_val + (cnt * delta) * direc)
--- micrOS/LM_neopixel.py
+++ micrOS/LM_neopixel.py
@@ -1,11 +1,17 @@
 from sys import platform
+from Common import transition
+from ConfigHandler import cfgget
+
+
 #########################################
 #       DIGITAL CONTROLLER PARAMS       #
 #########################################
-# Values: R, G, B, STATE_ON_OFF, IS_INITIALIZED
-__DCACHE = [100, 100, 100, 0]
-__NEOPIXEL_OBJ = None
-__PERSISTENT_CACHE = False
+class Data:
+    # Values: R, G, B, STATE_ON_OFF, IS_INITIALIZED
+    DCACHE = [100, 100, 100, 0]
+    NEOPIXEL_OBJ = None
+    PERSISTENT_CACHE = False
+    TRAN_OBJS = (None, None, None)
 
 
 #########################################
@@ -19,15 +25,14 @@ def __init_NEOPIXEL(n=24):
     n - number of led fragments
     n - must be set from code! (no persistent object handling in LMs)
     """
-    global __NEOPIXEL_OBJ
-    if __NEOPIXEL_OBJ is None:
+    if Data.NEOPIXEL_OBJ is None:
         from neopixel import NeoPixel
         from machine import Pin
         from LogicalPins import physical_pin
         neopixel_pin = Pin(physical_pin('neop'))     # Get Neopixel pin from LED PIN pool
-        __NEOPIXEL_OBJ = NeoPixel(neopixel_pin, n)                 # initialize for max 8 segments
+        Data.NEOPIXEL_OBJ = NeoPixel(neopixel_pin, n)                 # initialize for max 8 segments
         del neopixel_pin
-    return __NEOPIXEL_OBJ
+    return Data.NEOPIXEL_OBJ
 
 
 def __persistent_cache_manager(mode):
@@ -36,33 +41,31 @@ def __persistent_cache_manager(mode):
     modes:
         r - recover, s - save
     """
-    if not __PERSISTENT_CACHE:
+    if not Data.PERSISTENT_CACHE:
         return
-    global __DCACHE
     if mode == 's':
         # SAVE CACHE
         with open('neopixel.pds', 'w') as f:
-            f.write(','.join([str(k) for k in __DCACHE]))
+            f.write(','.join([str(k) for k in Data.DCACHE]))
         return
     try:
         # RESTORE CACHE
         with open('neopixel.pds', 'r') as f:
-            __DCACHE = [int(data) for data in f.read().strip().split(',')]
+            Data.DCACHE = [int(data) for data in f.read().strip().split(',')]
     except:
         pass
 
 
 def load_n_init(cache=None, ledcnt=24):
-    global __PERSISTENT_CACHE
     if cache is None:
-        __PERSISTENT_CACHE = False if platform == 'esp8266' else True
+        Data.PERSISTENT_CACHE = False if platform == 'esp8266' else True
     else:
-        __PERSISTENT_CACHE = cache
+        Data.PERSISTENT_CACHE = cache
     __persistent_cache_manager('r')        # recover data cache
     _ledcnt = __init_NEOPIXEL(n=ledcnt).n
-    if __PERSISTENT_CACHE and __DCACHE[3] == 1:
+    if Data.PERSISTENT_CACHE and Data.DCACHE[3] == 1:
         neopixel()                         # Set each LED for the same color
-    return "CACHE: {}, LED CNT: {}".format(__PERSISTENT_CACHE, _ledcnt)
+    return "CACHE: {}, LED CNT: {}".format(Data.PERSISTENT_CACHE, _ledcnt)
 
 
 def neopixel(r=None, g=None, b=None):
@@ -71,41 +74,39 @@ def neopixel(r=None, g=None, b=None):
     - Set all led fragments for the same color set
     - Default and cached color scheme
     """
-    global __DCACHE
-    r = __DCACHE[0] if r is None else r
-    g = __DCACHE[1] if g is None else g
-    b = __DCACHE[2] if b is None else b
+    r = Data.DCACHE[0] if r is None else r
+    g = Data.DCACHE[1] if g is None else g
+    b = Data.DCACHE[2] if b is None else b
     # Set each LED for the same color
     for element in range(0, __init_NEOPIXEL().n):    # Iterate over led string elements
-        __NEOPIXEL_OBJ[element] = (r, g, b)             # Set LED element color
-    __NEOPIXEL_OBJ.write()                              # Send data to device
+        Data.NEOPIXEL_OBJ[element] = (r, g, b)             # Set LED element color
+    Data.NEOPIXEL_OBJ.write()                              # Send data to device
     # Set cache
     if r > 0 or g > 0 or b > 0:
-        __DCACHE = [r, g, b, 1]                         # Cache colors + state (True-ON)
+        Data.DCACHE = [r, g, b, 1]                         # Cache colors + state (True-ON)
     else:
-        __DCACHE[3] = 0                                 # State - False - OFF
-    __persistent_cache_manager('s')                # Save cache - __DCACHE -  to file
+        Data.DCACHE[3] = 0                                 # State - False - OFF
+    __persistent_cache_manager('s')                # Save cache - Data.DCACHE -  to file
     return "NEOPIXEL SET TO R{}G{}B{}".format(r, g, b)
 
 
 def segment(r=None, g=None, b=None, s=0, cache=False, write=True):
-    global __DCACHE
-    r = __DCACHE[0] if r is None else r
-    g = __DCACHE[1] if g is None else g
-    b = __DCACHE[2] if b is None else b
+    r = Data.DCACHE[0] if r is None else r
+    g = Data.DCACHE[1] if g is None else g
+    b = Data.DCACHE[2] if b is None else b
     neo_n = __init_NEOPIXEL().n
     if s <= neo_n:
-        __NEOPIXEL_OBJ[s] = (r, g, b)
+        Data.NEOPIXEL_OBJ[s] = (r, g, b)
         # Send colors to neopixel
         if write:
-            __NEOPIXEL_OBJ.write()
+            Data.NEOPIXEL_OBJ.write()
         # Cache handling
         if cache:
             if r > 0 or g > 0 or b > 0:
-                __DCACHE = [r, g, b, 1]
+                Data.DCACHE = [r, g, b, 1]
             else:
-                __DCACHE[3] = 0
-            __persistent_cache_manager('s')  # Save cache - __DCACHE -  to file
+                Data.DCACHE[3] = 0
+            __persistent_cache_manager('s')  # Save cache - Data.DCACHE -  to file
 
         return "NEOPIXEL[{}] R{}G{}B{}".format(s, r, g, b)
     return "NEOPIXEL index error: {} > {}".format(s, neo_n)
@@ -116,12 +117,38 @@ def toggle(state=None):
     ON - OFF NeoPixel
     """
     if state is not None:
-        __DCACHE[3] = 0 if state else 1
-    if __DCACHE[3] == 1:
+        Data.DCACHE[3] = 0 if state else 1
+    if Data.DCACHE[3] == 1:
         neopixel(r=0, g=0, b=0)
         return "OFF"
-    neopixel(__DCACHE[0], __DCACHE[1], __DCACHE[2])
+    neopixel(Data.DCACHE[0], Data.DCACHE[1], Data.DCACHE[2])
     return "ON"
+
+
+def set_transition(r, g, b, sec):
+    timirqseq = cfgget('timirqseq')
+    from_red = Data.DCACHE[0]
+    from_green = Data.DCACHE[1]
+    from_blue = Data.DCACHE[2]
+    # Generate RGB color transition object (generator)
+    Data.TRAN_OBJS = (transition(from_val=from_red, to_val=r, step_ms=timirqseq, interval_sec=sec),
+                   transition(from_val=from_green, to_val=g, step_ms=timirqseq, interval_sec=sec),
+                   transition(from_val=from_blue, to_val=b, step_ms=timirqseq, interval_sec=sec))
+    return 'Settings was applied.'
+
+
+def run_transition():
+    if None not in Data.TRAN_OBJS:
+        try:
+            r = Data.TRAN_OBJS[0].__next__()
+            g = Data.TRAN_OBJS[1].__next__()
+            b = Data.TRAN_OBJS[2].__next__()
+            neopixel(int(r), int(g), int(b))
+            return 'Run R{}R{}B{}'.format(r, g, b)
+        except:
+            Data.TRAN_OBJS = (None, None, None)
+            return 'GenEnd.'
+    return 'Nothing to run.'
 
 
 #######################
@@ -130,4 +157,5 @@ def toggle(state=None):
 
 def help():
     return 'neopixel r=<0-255> g b n=<0-24)', 'toggle state=None', \
-           'load_n_init ledcnt=24', 'segment r, g, b, s=<0-n>'
+           'load_n_init ledcnt=24', 'segment r, g, b, s=<0-n>',\
+           'set_transition r=<0-255> g b sec', 'run_transition'

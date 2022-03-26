0cb0cf3d7933b49b5021fd2c2fac31f493ed4dbb
BNM <ban.marcell@gmail.com>
2022-03-26 22:14:11 +0100

    [DevToolKit] version update in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='micrOSDevToolKit',
-    version='0.1.4',
+    version='0.1.5',
     author='Marcell Ban',
     author_email='miros.framework@gmail.com',
     description='Development environment for micrOS (micropython based IoT solution)',

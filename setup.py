466e0172da867dfa8e131b309d98a7d8fbb35f94
BNM <ban.marcell@gmail.com>
2022-11-17 12:50:09 +0100

    [DevToolKit] dependency update: numpy, matplotlib

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='micrOSDevToolKit',
-    version='1.9.5',
+    version='1.9.6',
     author='Marcell Ban',
     author_email='miros.framework@gmail.com',
     description='Development environment for micrOS (micropython based IoT solution)',
@@ -23,7 +23,8 @@ setuptools.setup(
     license='MIT',
     packages=setuptools.find_packages(),
     install_requires=['adafruit-ampy==1.1.0', 'esptool==4.1', 'ipaddress', 'mpy-cross==1.19.1', 'netaddr',
-                      'netifaces', 'pylint', 'PyQt5', 'pyserial', 'resources', 'flask', 'flask_restful'],
+                      'netifaces', 'pylint', 'PyQt5', 'pyserial', 'resources', 'flask', 'flask_restful',
+                      'numpy', 'matplotlib'],
     scripts=['devToolKit.py'],
     include_package_data=True,
     use_scm_version=True,

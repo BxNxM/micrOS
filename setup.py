0edcdaad8c4968db6874f03f53f3d664937dd5b7
BNM <ban.marcell@gmail.com>
2022-11-20 14:22:45 +0100

    [DevToolKit] Dependency update with json2html

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='micrOSDevToolKit',
-    version='1.9.6',
+    version='1.9.7',
     author='Marcell Ban',
     author_email='miros.framework@gmail.com',
     description='Development environment for micrOS (micropython based IoT solution)',
@@ -24,7 +24,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=['adafruit-ampy==1.1.0', 'esptool==4.1', 'ipaddress', 'mpy-cross==1.19.1', 'netaddr',
                       'netifaces', 'pylint', 'PyQt5', 'pyserial', 'resources', 'flask', 'flask_restful',
-                      'numpy', 'matplotlib'],
+                      'numpy', 'matplotlib', 'json2html'],
     scripts=['devToolKit.py'],
     include_package_data=True,
     use_scm_version=True,

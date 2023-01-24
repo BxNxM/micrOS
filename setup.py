8df8cc97f65c91c2e5ce918409359de834ff0502
BNM <ban.marcell@gmail.com>
2023-01-24 18:27:46 +0100

    [micrOS] pip update - setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='micrOSDevToolKit',
-    version='2.8.4',
+    version='2.8.5',
     author='Marcell Ban',
     author_email='miros.framework@gmail.com',
     description='Development environment for micrOS (micropython based IoT solution)',

488c6136444c23f1fa84cfb2ad031b1728758e52
BNM <ban.marcell@gmail.com>
2023-08-09 13:32:46 +0200

    [micrOSGW] In container and published to docker hub + make.bash for automation

--- env/docker/make.bash
+++ env/docker/make.bash
@@ -1,9 +1,29 @@
 #!/bin/bash
-set -x
+#set -x
 
-echo -e "[CREATE IMAGE] docker build"
-docker build --no-cache -t micros-gateway:1.0 .
+VERSION=$1
+if [ "$VERSION" = "" ]
+then
+    echo -e "Missing version number"
+    exit 1
+fi
+
+IMAGE="bxnxm/micros-gateway:${VERSION}"
+
+echo -e "[CREATE IMAGE] docker build --no-cache -t ${IMAGE}"
+docker build --no-cache -t "${IMAGE}" .
 
 echo -e "[RUN IMAGE] docker run"
-docker run --name micros-gateway -p 5000:5000 -e GATEWAYIP="10.0.1.1" -d micros-gateway:1.0
+docker run --name micros-gateway -p 5000:5000 -e GATEWAYIP="10.0.1.1" -d "${IMAGE}"
 
+echo -n "DO YOU WANT TO PUBLISH THE IMAGE ${IMAGE} (Y/n): "
+read answer
+
+if [[ "$answer" == "Y" ]]; then
+    echo -e "\t[PUBLISH CONTAINER] docker push "${IMAGE}""
+    docker push "${IMAGE}"
+else
+    echo -e "\t[PUBLISH CONTAINER] SKIP docker push "${IMAGE}""
+fi
+
+exit 0

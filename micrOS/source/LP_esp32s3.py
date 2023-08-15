f1292e25bd54e2a8c63c9e931114d268366e8d03
BNM <ban.marcell@gmail.com>
2023-08-15 23:55:36 +0200

    [micrOS] Updated esp32s3 default pinmap (under validation) - version: 1.22.1-0

--- micrOS/source/LP_esp32s3.py
+++ micrOS/source/LP_esp32s3.py
@@ -3,17 +3,13 @@ from micropython import const
 # BUILTIN LED
 builtin = const(38)     # BUILT IN LED - progress_led
 
-###########################
-# TODO: review pins below #
-###########################
-
 # ANALOG RGB + WW + CW
 redgb = const(18)      # - rgb red channel [PWM]
 rgreenb = const(17)    # - rgb green channel [PWM]
 rgbue = const(16)      # - rgb blue channel [PWM]
 
 wwhite = const(15)	   # - warm white [PWM]
-cwhite = const(14)	   # - cold white [PWM]
+cwhite = const(7)	   # - cold white [PWM]
 
 
 # DIGITAL LED
@@ -27,18 +23,18 @@ servo_2 = const(12)    # - servo(2) [PWM]
 switch_1 = const(11)   # - switch(1) [simple]
 switch_2 = const(10)   # - switch(2) [simple]
 switch_3 = const(9)    # - switch(3) [simple]
-switch_4 = const(8)    # - switch(4) [simple]
+switch_4 = const(46)    # - switch(4) [simple]
 
-dim_1 = const(7)      # - dimmer(1) [PWM]
+dim_1 = const(3)      # - dimmer(1) [PWM]
 
-l298speed = const(46)  # - DC motor pwm control [PWM]
-l298dir_1 = const(45)  # - DC motor direction (1)
-l298dir_2 = const(44)  # - DC motor direction (2)
+l298speed = const(4)  # - DC motor pwm control [PWM]
+l298dir_1 = const(5)  # - DC motor direction (1)
+l298dir_2 = const(6)  # - DC motor direction (2)
 
-l9110dir_1 = const(37) # - DC motor direction (1)
-l9110dir_2 = const(36) # - DC motor direction (2)
+l9110dir_1 = const(4) # - DC motor direction (1)
+l9110dir_2 = const(5) # - DC motor direction (2)
 
-buzzer = const(43)     # - Buzzer pin - sound generator
+buzzer = const(42)     # - Buzzer pin - sound generator
 
 
 stppr_1 = const(35)    # - stepper motor pin
@@ -53,21 +49,21 @@ i2c_scl = const(47)    # - oled - clock
 
 
 # EXTERNAL EVENT IRQ
-irq1 = const(0)        # - event irq pin
-irq2 = const(1)        # - event irq pin
-irq3 = const(2)        # - event irq pin
-irq4 = const(3)        # - event irq pin
-oleduibttn = const(4)  # - oled_ui center/ok button
+irq1 = const(41)        # - event irq pin
+irq2 = const(40)        # - event irq pin
+irq3 = const(39)        # - event irq pin
+irq4 = const(37)        # - event irq pin
+oleduibttn = const(45)  # - oled_ui center/ok button
 
-touch_0 = const(5)     # - touch sensor TODO
+touch_0 = const(0)     # - touch sensor TODO
 
 
 # SENSORS
-hcsrtrig = const(42)   # - distance HCSR04 trigger pin
-hcsrecho = const(41)   # - distance HCSR04 echo pin
-dhtpin = const(46)     # - dht_pin 11 and 22
-co2 = const(45)        # - [ADC] CO2
-temp6000 = const(44)   # - [ADC] light sensor TEMP6000
-ph = const(37)         # - [ADC] PH sensor
-ds18 = const(36)       # - DS18B20 - temp. sensor
+hcsrtrig = const(1)   # - distance HCSR04 trigger pin
+hcsrecho = const(2)   # - distance HCSR04 echo pin
+dhtpin = const(20)     # - dht_pin 11 and 22
+co2 = const(21)        # - [ADC] CO2
+temp6000 = const(35)   # - [ADC] light sensor TEMP6000
+ph = const(36)         # - [ADC] PH sensor
+ds18 = const(35)       # - DS18B20 - temp. sensor
 mic = const(5)         # - [ADC] microphone

1f6a47285df8630001710f9eb746333b778efb7a
BNM <ban.marcell@gmail.com>
2023-11-23 10:37:20 +0100

    [micrOS] urequests.py module refactor (prepare for async implementation)

--- micrOS/source/urequests.py
+++ micrOS/source/urequests.py
@@ -44,40 +44,21 @@ def _chunked(data):
     return decoded
 
 
-def request(method, url, data=None, json=None, headers=None, sock_size=1024, jsonify=False):
-    """
-    Micropython HTTP request function for REST API handling
-    :param method: GET/POST
-    :param url: URL for REST API
-    :param data: string body (handle bare string as data for POST method)
-    :param json: json body (handle json as data for POST method)
-    :param headers: define headers
-    :param sock_size: socket buffer size, default 1024 byte (micropython)
-    :param jsonify: convert response body to json
-    """
-
+def _parse_url(url, headers=None):
     if headers is None:
         headers = {}
-
-    # [1] PARSE URL -> proto (http/https), host, path + SET PORT
+    # PARSE URL -> proto (http/https), host, path + SET PORT
     proto, _, host, path = url.split('/', 3)
     port = 443 if proto == 'https:' else 80
-    # [1.1] PARSE HOST - handle direct port number as input after :
+    # PARSE HOST - handle direct port number as input after :
     if ':' in host:
         host, port = host.split(':', 1)
         port = int(port)
+    return proto, host, port, path, headers
 
-    # [2] CONNECT - create socket object
-    sock = socket()
-    sock.settimeout(2)
-    # [2.1] CONNECT - resolve IP by host
-    addr = getaddrinfo(host, port)[0][-1]
-    # [2.2] CONNECT - if https handle ssl
-    sock.connect(addr)
-    if proto == 'https:':
-        sock = wrap_socket(sock)
 
-    # [3] BUILD REQUEST: body, headers
+def _build_request(data, headers, method, path, host, json):
+    # BUILD REQUEST: body, headers
     if data is not None:
         body = data.encode('utf-8')
         headers['Content-Length'] = len(body)
@@ -96,8 +77,50 @@ def request(method, url, data=None, json=None, headers=None, sock_size=1024, jso
     http_request = '\r\n'.join(lines) + '\r\n\r\n'
     if body is not None:
         http_request += body.decode('utf-8')
+    return http_request
 
-    # [4] SEND REQUEST
+
+def _parse_response(response):
+    # PARSE RESPONSE
+    headers, body = response.split(b'\r\n\r\n', 1)
+    status_code = int(headers.split(b' ')[1])
+    headers = dict(h.split(b': ') for h in headers.split(b'\r\n')[1:])
+    if headers.get(b'Transfer-Encoding', b'') == b'chunked':
+        body = _chunked(body)
+    else:
+        body = body.decode('utf-8')
+    return status_code, body
+
+
+def request(method, url, data=None, json=None, headers=None, sock_size=1024, jsonify=False):
+    """
+    Micropython HTTP request function for REST API handling
+    :param method: GET/POST
+    :param url: URL for REST API
+    :param data: string body (handle bare string as data for POST method)
+    :param json: json body (handle json as data for POST method)
+    :param headers: define headers
+    :param sock_size: socket buffer size, default 1024 byte (micropython)
+    :param jsonify: convert response body to json
+    """
+
+    # Parse URL
+    proto, host, port, path, headers = _parse_url(url, headers)
+
+    # [1] CONNECT - create socket object
+    sock = socket()
+    sock.settimeout(2)
+    # [1.1] CONNECT - resolve IP by host
+    addr = getaddrinfo(host, port)[0][-1]
+    # [1.2] CONNECT - if https handle ssl
+    sock.connect(addr)
+    if proto == 'https:':
+        sock = wrap_socket(sock)
+
+    # Create request
+    http_request = _build_request(data, headers, method, path, host, json)
+
+    # [2] SEND REQUEST
     if proto == 'https:':
         sock.write(http_request.encode('utf-8'))    # Send request (secure)
         receive = sock.read                         # Save Read object (secure)
@@ -106,7 +129,7 @@ def request(method, url, data=None, json=None, headers=None, sock_size=1024, jso
         sock.send(http_request.encode('utf-8'))     # Send request
         receive = sock.recv                         # Save Read object
 
-    # [5] RECEIVE RESPONSE
+    # [3] RECEIVE RESPONSE
     response = receive(sock_size)
     while True:
         data = receive(sock_size)
@@ -116,14 +139,9 @@ def request(method, url, data=None, json=None, headers=None, sock_size=1024, jso
         #print("RAW DATA STREAM: {}".format(data))
     sock.close()
 
-    # [6] PARSE RESPONSE
-    headers, body = response.split(b'\r\n\r\n', 1)
-    status_code = int(headers.split(b' ')[1])
-    headers = dict(h.split(b': ') for h in headers.split(b'\r\n')[1:])
-    if headers.get(b'Transfer-Encoding', b'') == b'chunked':
-        body = _chunked(body)
-    else:
-        body = body.decode('utf-8')
+    # Parse response
+    status_code, body = _parse_response(response)
+
     # Return status code, headers and body (text or jsons)
     return status_code, loads(body) if jsonify else body
 

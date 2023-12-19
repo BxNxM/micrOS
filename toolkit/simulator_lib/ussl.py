6f50035d6d034902fd38791218d3bbc6d143d39d
BNM <ban.marcell@gmail.com>
2023-12-19 14:42:06 +0100

    [devToolKit] fix simulator python3.12 ssl incompatibility with socket_wrap in micropython over simulator_lib/ussl.py

--- toolkit/simulator_lib/ussl.py
+++ toolkit/simulator_lib/ussl.py
@@ -4,5 +4,13 @@ def wrap_socket(sock):
         from ssl import wrap_socket
         return wrap_socket(sock)
     except Exception as e:
-        print(f"[SIMULATOR] WARNING - SSL INTERFACE CHANCE IN PYTHON 3.12...: {e}")
-        return sock
+        print(f"[SIMULATOR][micropython DIFF] WARNING(1/2) - SSL INTERFACE CHANCE IN PYTHON 3.12...: {e}")
+    try:
+        from ssl import create_default_context, CERT_NONE
+        context = create_default_context()
+        context.check_hostname = False
+        context.verify_mode = CERT_NONE
+        return context.wrap_socket(sock)
+    except Exception as e:
+        print(f"[SIMULATOR][WRAP] ERROR(2/2) - SSL INTERFACE CHANCE IN PYTHON 3.12...: {e}")
+    return sock

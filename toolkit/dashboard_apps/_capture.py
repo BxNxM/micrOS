f77ba106021b46de36cb5655549f0f1178723992
BNM <ban.marcell@gmail.com>
2023-12-20 16:34:02 +0100

    Capture dashboard_apps/_capture.py improvement

--- toolkit/dashboard_apps/_capture.py
+++ toolkit/dashboard_apps/_capture.py
@@ -3,10 +3,25 @@ import requests
 import numpy as np
 import time
 import sys
+import socket
+
+RESOLVED_URLS = {}
 
 def capture_image(url):
+    # Resolve and cache hostname (IP address)
     try:
-        response = requests.get(url)
+        # Extracting the hostname from the URL
+        hostname = url.split('/')[2]
+        # Resolving the IP address
+        ip_address = socket.gethostbyname(hostname)
+        resolved_url = url.replace(hostname, ip_address)
+        RESOLVED_URLS[url] = resolved_url
+    except Exception as e:
+        print(f"URL has no hostname: {url} - fallback to u resolved url: {e}")
+        RESOLVED_URLS[url] = url            # fallback
+    # Run request...
+    try:
+        response = requests.get(RESOLVED_URLS[url], timeout=5)
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             # Convert the binary image data to a NumPy array
@@ -16,6 +31,8 @@ def capture_image(url):
             return img
         else:
             print(f"Error: Unable to fetch image. Status code: {response.status_code}")
+    except requests.Timeout:
+        print("Error: The request timed out")
     except Exception as e:
         print(f"Error: {e}")
     return None
@@ -41,6 +58,10 @@ def main(url):
             total_time_count += elapsed_time
             fps = frame_count / total_time_count        # average FPS: total frame count / total receive time
             cv2.putText(image, f"FPS: {fps:.2f} {elapsed_time:.1f}s", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            # Getting the resolution information
+            height, width, _ = image.shape
+            #print(f"Image resolution: {width}x{height}, Channels: {channels}")
+            cv2.putText(image, f"{height}x{width}", (width-80, height-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
             # Display the image
             cv2.imshow(url, image)
 
@@ -57,6 +78,6 @@ if __name__ == "__main__":
     devfid = '10.0.1.106'       # Default for test
     if len(args) > 1:
         devfid = args[1]
-    url = f"http://{devfid}/cam/"
+    url = f"http://{devfid}.local/cam/"
     main(url=url)
 

c99ce5b4c471f02d4a49f32ea6d6204bf1cd5f47
BNM <ban.marcell@gmail.com>
2024-01-27 23:08:54 +0100

    [micrOS] LM_keychain.py beta - version: 1.54.0-1

--- micrOS/source/LM_keychain.py
+++ micrOS/source/LM_keychain.py
@@ -1,33 +1,54 @@
-import LM_oled as oled
+from LM_oled import text, show, load_n_init as oled_lni
 from LM_ds18 import measure
 from LM_neopixel import color, brightness, toggle
 
 INITED = False
 
 def load_n_init():
+    """
+    Init OLED display 64x32
+    """
     global INITED
-    oled.load_n_init(64, 32)
-    INITED = True
+    try:
+        oled_lni(64, 32)
+        INITED = True
+        return f'OLED INIT OK'
+    except Exception as e:
+        INITED = False
+        return f'OLED INIT NOK: {e}'
 
 
 def display():
-    # STA    H:M:S  W
-    #    IP:    1.92
-    #    Temp:  40C
-    if not INITED:
-        load_n_init()
+    """
+    Run display content refresh
+        STA    H:M:S  W
+          IP:   1.92
+          T[C]: 40C
+    """
 
-    oled.text('STA 12:0:0', x=1, y=1)
-    oled.text('IP: 1.92', x=4, y=10)
-    oled.text(f"T[C]: {measure()}", x=4, y=20)
-    oled.show()
+    if not INITED:
+        _v = load_n_init()
+        if not INITED:
+            return _v
+
+    text('STA 12:0:0', x=0, y=1)
+    text('IP: 1.92', x=4, y=10)
+    text(f"T[C]: {measure()}", x=4, y=20)
+    show()
+    return 'Display show'
 
 
 def temperature():
+    """
+    Measure ds18B20 temperature sensor
+    """
     return measure()
 
 
 def neopixel(r=None, g=None, b=None, br=None, onoff=None, smooth=True):
+    """
+    Set neopixel LED colors
+    """
     if r is not None or g is not None or b is not None:
         return color(r, g, b, smooth=smooth)
     if br is not None:
@@ -37,13 +58,30 @@ def neopixel(r=None, g=None, b=None, br=None, onoff=None, smooth=True):
             return toggle(smooth=smooth)
         state = True if onoff == 'on' else False
         return toggle(state, smooth=smooth)
+    return 'No action: r g b br onoff smooth'
 
 
 def lmdep():
+    """
+    Load Module dependencies
+    """
     return 'oled', 'ds18', 'neopixel'
 
 
+def pinmap():
+    """
+    PIN MAP dump
+    """
+    from LM_oled import pinmap as o_pmp
+    from LM_ds18 import pinmap as t_pmp
+    from LM_neopixel import pinmap as n_pmp
+    pmp = o_pmp()
+    pmp.update(t_pmp())
+    pmp.update(n_pmp())
+    return pmp
+
+
 def help():
-    return 'load_n_init', 'temperature', 'display', 'display &&1000'\
+    return 'load_n_init', 'temperature', 'display', 'display &&1000',\
            'neopixel r=<0-255> g=<0-255> b=<0-255> br=<0-100> onoff=<toggle/on/off> smooth=<True/False>',\
-           'lmdep'
+           'pinmap', 'lmdep'
--- micrOS/source/Shell.py
+++ micrOS/source/Shell.py
@@ -24,7 +24,7 @@ from Debug import console_write, errlog_add
 #################################################################
 
 class Shell:
-    MICROS_VERSION = '1.54.0-0'
+    MICROS_VERSION = '1.54.0-1'
 
     def __init__(self):
         """

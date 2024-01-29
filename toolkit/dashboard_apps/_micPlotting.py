2bdea34da351d20c3b0c61a8cb2fc62aa5773b48
szeka9 <szkalman79@gmail.com>
2024-01-29 22:01:58 +0100

    [micrOS][devToolKit] Fix issues in PR #27
    Rewrite dashboard app for I2S microphone
    to eliminate performance issues, and fix
    issues described in PR comments.

--- toolkit/dashboard_apps/_micPlotting.py
+++ toolkit/dashboard_apps/_micPlotting.py
@@ -5,126 +5,167 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import io
 import soundfile as sf
-import numpy as np
 import re
-import urllib3
+import requests
 import threading
 import time
+import pyaudio
 
 from collections import deque
 
-RECORD = True           # Indiciate if threads should be terminated
-STREAM_THREAD = None    # Thread for processing the multipart stream
-DECODING_THREAD = None  # Thread for decoding raw bytes
-
-SAMPLE_RATE = None
-NUM_CHANNELS = None
-RAW_DATA = deque()      # Raw data (bytes) passed from HTTP stream
-AUDIO_DATA = []         # Decoded audio for visualization
-AUDIO_DATA_BYTES = []   # Audio data in byte format, to be saved as a file
-ENCODING = 'PCM_16'
-READ_SIZE = 8192
+OUTPUT_DEVICE = 10      # use pyaudio.PyAudio().get_default_output_device_info() to get default device index
+ENCODING = 'PCM_16'     # 16-bit PCM is used for audio/l16
+PLOT_INTERVAL_MS = 50   # iterval between plot updates
+PLOT_MAX_LEN = 40000    # maximum number of samples to display on the plot
+MULTIPART_BOUNDARY = b'\r\n--micrOS_boundary\r\n'   # multipart boundary to search for when reading the stream
 
 
-def create_multipart_stream(url):
-    stream = urllib3.request('GET',url,preload_content=False)
-    reader = io.BufferedReader(stream)
-    return reader
+def determine_parameters(reader):
+    retry_counter = 100
+    for line in reader:
+        if retry_counter <= 0:
+            break
+
+        retry_counter -= 1
+        header = line.find(b'Content-Type: audio/l16')
+
+        if header == -1:
+            continue
+
+        # Determine sampling parameters from content-type
+        m_r = re.search(b'rate=(\d+)', line)
+        m_c = re.search(b'channels=(\d)', line)
+
+        if not m_r or not m_c:
+            raise Exception('Invalid content-type, could not determine sample rates or channels.')
+
+        sample_rate = int(m_r.group(1))
+        num_channels = int(m_c.group(1))
+
+        print(f'Sampling rate: {sample_rate}, number of channels: {num_channels}')
+
+        # Play live audio on the device corresponding to OUTPUT_DEVICE index
+        pyaudio_player = pyaudio.PyAudio()
+        pyaudio_stream = pyaudio_player.open(format=pyaudio.paInt16,
+                                                channels=num_channels,
+                                                rate=sample_rate,
+                                                output_device_index=OUTPUT_DEVICE,
+                                                output=True)
+
+        return sample_rate, num_channels, pyaudio_player, pyaudio_stream
+    raise Exception('Could not determine sampling parameters')
 
 
-def run_stream(reader):
-    global RAW_DATA
-    global NUM_CHANNELS
-    global SAMPLE_RATE
+def run_stream(reader, terminate_event, raw_data_queue):
+    """
+    Read multipart stream
+    """
+    for line in reader:
+        line = line.lstrip(b'Content-Type: audio/l16;rate=8000;channels=1\r\n\r\n')
+        if len(line):
+            raw_data_queue.append(line)
 
-    while RECORD:
-        current_line = reader.readline(READ_SIZE).rstrip(b'\r\n')
-        boundary = current_line.find(b'--micrOS_boundary')
-        header = current_line.find(b'Content-Type: audio/l16')
-
-        if header != -1 and (not SAMPLE_RATE or not NUM_CHANNELS):
-            # Determine sampling parameters from content-type
-            m_r = re.search(b'rate=(\d+)', current_line)
-            m_c = re.search(b'channels=(\d)', current_line)
-            if not m_r or not m_c:
-                raise Exception('Invalid content-type, could not determine sample rates or channels.')
-            SAMPLE_RATE = int(m_r.group(1))
-            NUM_CHANNELS = int(m_c.group(1))
-            print(f'Sampling rate: {SAMPLE_RATE}, number of channels: {NUM_CHANNELS}')
-
-        if len(current_line) and boundary == -1 and header == -1:
-            # The line contains audio data
-            RAW_DATA.append(current_line)
+        if terminate_event.is_set():
+            break
 
 
-# Decode audio bytes (PCM_16) to floats (-1 to 1)
-def decode_audio_bytes(audio_bytes):
+def decode_audio_bytes(audio_bytes, sample_rate, num_channels, encoding):
+    """
+    Decode audio bytes (PCM_16) to floats (-1 to 1)
+    """
     data, _ = sf.read(io.BytesIO(audio_bytes),
                       format='RAW',
-                      samplerate=SAMPLE_RATE,
-                      channels=NUM_CHANNELS,
-                      subtype=ENCODING,
+                      samplerate=sample_rate,
+                      channels=num_channels,
+                      subtype=encoding,
                       endian='LITTLE')
     return data
 
 
-def decode_audio():
-    global RAW_DATA
-    global AUDIO_DATA
-    global AUDIO_DATA_BYTES
+def decode_audio(terminate_event, raw_data_queue, raw_audio, decoded_audio,
+                 sample_rate, num_channels, encoding, pyaudio_stream):
+    while not terminate_event.is_set():
+        counter = 100 # How many segments to process at once
 
-    while RECORD:
-        counter = 10 # How many segments to process at once
-        while len(RAW_DATA) and counter > 0:
-            new_data = RAW_DATA.popleft()
-            AUDIO_DATA_BYTES.extend(new_data)
-            AUDIO_DATA.extend(decode_audio_bytes(new_data))
+        new_segment = bytes()
+        while len(raw_data_queue) and counter > 0:
+            new_segment += raw_data_queue.popleft()
             counter -= 1
-        time.sleep(0.01)
+
+        if pyaudio_stream is not None and pyaudio_stream.is_active():
+            pyaudio_stream.write(new_segment)
+
+        raw_audio.extend(new_segment)
+        decoded_audio.extend(decode_audio_bytes(new_segment, sample_rate, num_channels, encoding))
 
 
-def start_stream(url):
-    global STREAM_THREAD
-    global DECODING_THREAD
+def start_stream(reader):
+    sample_rate, num_channels, pyaudio_player, pyaudio_stream = determine_parameters(reader)
 
+    terminate_event = threading.Event()         # Event object for terminating threads
+    raw_data_queue = deque()                    # Raw data (bytes) passed from HTTP stream
+    decoded_audio = deque(maxlen=PLOT_MAX_LEN)  # Decoded audio for visualization
+    raw_audio = bytearray()                     # Audio data in byte format, to be saved as a file
+
+    # Thread for processing the multipart stream
+    stream_thread = threading.Thread(target=run_stream,
+                                     args=(reader, terminate_event, raw_data_queue))
+    stream_thread.start()
+
+    # Thread for decoding raw bytes
+    decoding_thread = threading.Thread(target=decode_audio,
+                                       args=(terminate_event, raw_data_queue,
+                                             raw_audio, decoded_audio,
+                                             sample_rate, num_channels,
+                                             ENCODING, pyaudio_stream))
+    decoding_thread.start()
+
+    # Plotting animation
     fig, ax = plt.subplots()
-    stream = create_multipart_stream(url)
-    STREAM_THREAD = threading.Thread(target=run_stream, args=(stream,))
-    STREAM_THREAD.start()
-    DECODING_THREAD = threading.Thread(target=decode_audio)
-    DECODING_THREAD.start()
 
     def update(i):
-        current_length = len(AUDIO_DATA)
-        if current_length:
+        if len(decoded_audio):
             ax.clear()
-            plt.plot(range(current_length), AUDIO_DATA[:current_length])
+            plt.plot(range(len(decoded_audio)), decoded_audio)
             plt.grid()
-            plt.ylim([-1*np.max(AUDIO_DATA[:current_length])*1.3,np.max(AUDIO_DATA[:current_length])*1.3])
-            plt.xlim([current_length-SAMPLE_RATE*3,current_length])
-            plt.title('Microphone recording', fontsize=12, fontweight='bold')
+            plt.ylim([-1,1])
+            plt.title('Audio waveform', fontsize=12, fontweight='bold')
             plt.ylabel("Amplitude")
             plt.xlabel("Sample")
 
             ax.text(0.01, 0.02,
-                "${f_{sampling}}$ [Hz] = "f"{SAMPLE_RATE:.2f}",
+                "${f_{sampling}}$ [Hz] = "f"{sample_rate:.2f}",
                 transform=ax.transAxes,
                 fontsize=10
                 )
 
-    ani = anim.FuncAnimation(fig, update, repeat=True, interval=50)
+    animation = anim.FuncAnimation(fig, update, repeat=True, interval=PLOT_INTERVAL_MS)
     plt.show()
+
+    # Terminate threads and streams when the user closes the window
+    terminate_event.set()
+    stream_thread.join()
+    decoding_thread.join()
+    pyaudio_player.terminate()
+    pyaudio_stream.stop_stream()
+    pyaudio_stream.close()
+
+    # Save recorded audio into a WAV file
+    if len(decoded_audio):
+        wav_file = f'./toolkit/user_data/{int(time.time()*1000)}.wav'
+        sf.write(file=wav_file,
+                 data=decode_audio_bytes(raw_audio, sample_rate, num_channels, ENCODING),
+                 format='WAV',
+                 samplerate=sample_rate,
+                 subtype=ENCODING,
+                 endian='LITTLE')
+        print(f'WAV file saved to: {wav_file}')
 
 
 def app(devfid=None):
     """
     devfid: selected device input
-    frames: number of measurements to take
-    window_size: maximum number of data points displayed at once
-    repeat: measure continuously
     """
-    global RECORD
-
     # Handle command line arguments
     args = sys.argv
     print("Start python script: {}".format(args[0]))
@@ -134,20 +175,14 @@ def app(devfid=None):
         devfid = args[1]
 
     url = f"http://{devfid}/mic/stream"
-    start_stream(url)
-    RECORD = False
-    STREAM_THREAD.join()
-    DECODING_THREAD.join()
+
+    stream = requests.get(url,stream=True)
+    reader = stream.iter_lines(delimiter=MULTIPART_BOUNDARY)
+    try:
+        start_stream(reader)
+    finally:
+        stream.close()
 
 
 if __name__ == "__main__":
     app()
-    if len(AUDIO_DATA):
-        wav_file = f'./toolkit/user_data/{int(time.time()*1000)}.wav'
-        sf.write(file=wav_file,
-                 data=AUDIO_DATA,
-                 format='WAV',
-                 samplerate=SAMPLE_RATE,
-                 subtype=ENCODING,
-                 endian='LITTLE')
-        print(f'WAV file saved to: {wav_file}')

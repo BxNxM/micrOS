3583f30c1334b96eab2a3ad3d015168df66a130d
BNM <ban.marcell@gmail.com>
2024-02-04 20:06:54 +0100

    [DevEnv][PYPI] MANIFEST.in precompile corner case fix - package: 1.56.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ dependencies = ['adafruit-ampy', 'mpremote', 'esptool', 'ipaddress', 'netaddr',
 
 setuptools.setup(
     name='micrOSDevToolKit',
-    version='1.56.0',
+    version='1.56.1',
     author='Marcell Ban',
     author_email='miros.framework@gmail.com',
     description='Development and deployment environment for micrOS, the diy micropython automation OS (IoT)',

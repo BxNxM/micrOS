4b27253d02acb089302890f9c5dabd89863fd539
BNM <ban.marcell@gmail.com>
2024-02-14 20:17:23 +0100

    [PYPI] update - package: 1.59.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ dependencies = ['adafruit-ampy', 'mpremote', 'esptool', 'ipaddress', 'netaddr',
 
 setuptools.setup(
     name='micrOSDevToolKit',
-    version='1.57.0',
+    version='1.59.0',
     author='Marcell Ban',
     author_email='miros.framework@gmail.com',
     description='Development and deployment environment for micrOS, the diy micropython automation OS (IoT)',

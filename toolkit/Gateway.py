35f1234b3510d2f0b3f9fd59f80e15b0a0fbbc84
BNM <ban.marcell@gmail.com>
2024-02-16 16:31:41 +0100

    [Gateway] fix (local ip filtering) - package: 1.59.2

--- toolkit/Gateway.py
+++ toolkit/Gateway.py
@@ -61,7 +61,7 @@ if BasicAuth is not None and (__rest_usr_name and __rest_usr_pwd):
 
     def _is_local_network():
         # Define local network IP prefixes (adjust as needed)
-        local_network_prefixes = ['192.168.', '10.0.', '172.17.']
+        local_network_prefixes = ['192.168.', '10.0.', '172.']
         #local_network_prefixes = []
         remote_ip = request.remote_addr
         print(f"\t[GW-AUTH] Check incoming IP address: {remote_ip}")

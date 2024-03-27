14750156fe02acd9d6cbe766e4d723c81e46fc3a
BNM <ban.marcell@gmail.com>
2024-03-27 01:01:30 +0100

    [micrOS] LM_keychain.py update for first hw prototype (without built in touch sensor - not enough space) - version: 2.0.4-1

--- micrOS/source/LM_keychain.py
+++ micrOS/source/LM_keychain.py
@@ -4,7 +4,9 @@ from utime import localtime
 from Network import ifconfig
 from LM_oled import text, show, rect, pixel, clean, load_n_init as oled_lni
 from LM_ds18 import measure
-from LM_neopixel import color, brightness, toggle, load_n_init as neopixel_lni
+from microIO import physical_pin, pinmap_dump
+from neopixel import NeoPixel
+from machine import Pin
 try:
     from LM_gameOfLife import next_gen as gol_nextgen, reset as gol_reset
 except:
@@ -12,12 +14,15 @@ except:
 
 class KC:
     INITED = False
-    DP_main_page = True          # store display logical state: ON (main page) / OFF
+    DP_main_page = True     # store display logical state: ON (main page) / OFF
     DP_cnt = 0              # display "count back", when 0 go to sleep -> OFF
     DP_cnt_default = None   # store calculated sequence to sleep 30sec/period_ms
+    NEOPIXEL_OBJ = None     # Neopixel handler
+    COLOR_WHEEL = None      # Neopixel color wheel animation
+    NEOPIXEL_BR = 30        # default neopixel brightness
 
 
-def _screen_saver(scale=2):
+async def _screen_saver(scale=2):
     """
     Conway's game of life screen saver simulation
     :param scale: default (2) game of life matrix (16x32) upscale to real display size 32x64
@@ -30,6 +35,9 @@ def _screen_saver(scale=2):
     if matrix is None:
         # Reset Game of life
         gol_reset()
+        # flash main page - quick view
+        await _main_page()                  # Run main screen page coroutine
+        await asyncio.sleep_ms(1500)        # Wait 1,5s to show main screen before continue screensaver
     else:
         # Update display with Conway's Game of Life
         clean()
@@ -64,7 +72,10 @@ async def _main_page(vd=False):
     # Draw data to display
     text(f"{h}:{m}:{s}", x=0, y=1)
     text(f"{nwmd}: {devip}", x=4, y=10)
-    text(f"{round(tuple(measure().values())[0], 1)} C", x=4, y=20)
+    try:
+        text(f"{round(tuple(measure().values())[0], 1)} C", x=4, y=20)
+    except Exception:
+        pass
     show()
     return "Display show"
 
@@ -83,7 +94,8 @@ async def __task(period_ms, vd=False):
 
     KC.DP_cnt_default = int(30_000 / period_ms)     # After 30 sec go to sleep mode
     KC.DP_cnt = KC.DP_cnt_default                   # Set sleep counter
-    fast_period = int(period_ms/2)                  # Calculate faster refresh period
+    fast_period = int(period_ms/4)                  # Calculate faster refresh period
+    fast_period = fast_period if fast_period > 100 else 100
 
     # Run keychain main async loop, with update ID: kc._display
     with micro_task(tag="kc._display") as my_task:
@@ -97,7 +109,7 @@ async def __task(period_ms, vd=False):
                 await asyncio.sleep_ms(period_ms)
             else:
                 # [SLEEP MODE] Execute screen saver page
-                _screen_saver()                             # Run sleep page function
+                await _screen_saver()                       # Run sleep page function
                 # Async sleep - feed event loop
                 await asyncio.sleep_ms(fast_period)
 
@@ -107,7 +119,8 @@ async def __task(period_ms, vd=False):
                 clean()                         #2 clean screen
                 show()                          #3 show cleaned display
                 KC.DP_cnt = KC.DP_cnt_default   #4 reset sleep counter to default
-                my_task.out = 'sleep...'        
+                my_task.out = 'sleep...'
+            neopixel_color_wheel()              # update neopixel color wheel
 
 
 def _boot_page(msg):
@@ -126,7 +139,7 @@ def load_n_init(width=64, height=32, bootmsg="micrOS"):
     :param height: screen height (pixel)
     :param bootmsg: First text on page at bootup, default: "micrOS"
     """
-    neopixel_lni(ledcnt=1)                  #1 Init neopixel
+    KC.COLOR_WHEEL = _color_wheel()         #1 Init neopixel color wheel generator
     try:
         oled_lni(width, height)             #2 Init oled display
         _boot_page(bootmsg)                 #3 Show boot page text
@@ -143,9 +156,9 @@ def display(period=1000, vd=False):
     :param vd: virtual display 64x32 (rectangle for bigger screens)
     """
     # [!] ASYNC TASK CREATION [1*] with async task callback + taskID (TAG) handling
-    period_ms = 1000 if period < 1000 else period
+    period_ms = 500 if period < 500 else period
     state = micro_task(tag="kc._display", task=__task(period_ms=period_ms, vd=vd))
-    return "Starting" if state else "Already running"
+    return f"Starting" if state else "Already running"
 
 
 def temperature():
@@ -155,21 +168,41 @@ def temperature():
     return measure()
 
 
-def neopixel(r=None, g=None, b=None, br=None, onoff=None, smooth=True):
+def _color_wheel():
     """
-    Set neopixel LED colors
+    Neopixel color wheel generator
     """
-    if r is not None or g is not None or b is not None:
-        return color(r, g, b, smooth=smooth)
-    if br is not None:
-        brightness(br, smooth=smooth)
-    if onoff is not None:
-        if onoff == "toggle":
-            return toggle(smooth=smooth)
-        state = onoff == 'on'
-        return toggle(state, smooth=smooth)
-    return 'No action: r g b br onoff smooth'
+    max_val = 10                    # normally up to 255, but must be limited due to heat problems (4%)
+    while True:
+        for r in range(0, max_val, 2):
+            for g in range(0, max_val, 2):
+                for b in range(0, max_val, 2):
+                    yield r, g, b
 
+
+def neopixel_color_wheel(br=KC.NEOPIXEL_BR):
+    """
+    Neopixel color wheel
+    :param br: brightness value 0-100 percent
+    :param run: run led update / disable
+    """
+    def _init():
+        if KC.NEOPIXEL_OBJ is None:
+            neopixel_pin = Pin(physical_pin('neop'))        # Get Neopixel pin from LED PIN pool
+            KC.NEOPIXEL_OBJ = NeoPixel(neopixel_pin, n=1)   # initialize for max 1 segment
+        return KC.NEOPIXEL_OBJ
+
+    neo_led = _init()
+    if br != KC.NEOPIXEL_BR:
+        KC.NEOPIXEL_BR = br                         # update neopixel brightness cache
+    if KC.COLOR_WHEEL is None:
+        KC.COLOR_WHEEL = _color_wheel()             # init color wheel generator
+    r, g, b = next(KC.COLOR_WHEEL)                  # get next color
+    br = br if br == 100 else br/100                # calculate brightness multiplier
+    r, g, b = int(r*br), int(g*br), int(b*br)       # apply brightness parameter
+    neo_led[0] = (r, g, b)                          # Set LED element color
+    neo_led.write()                                 # Send data to LED device
+    return {'R': r, 'G': g, 'B': b, 'S': 1}         # Return verdict
 
 def press_event():
     """
@@ -181,8 +214,9 @@ def press_event():
     if KC.DP_main_page:
         # Reset display counter
         KC.DP_cnt = KC.DP_cnt_default
-        # Neopixel toggle
-        toggle()
+        # Neopixel color wheel disable/enable
+        brightness = 0 if KC.NEOPIXEL_BR > 0 else 30        # set default brightness 30% when turns on
+        neopixel_color_wheel(br=brightness)
         return
     # Wake display
     KC.DP_main_page = True
@@ -192,7 +226,7 @@ def lmdep():
     """
     Load Module dependencies
     """
-    return 'oled', 'ds18', 'neopixel', 'gameOfLife'
+    return 'oled', 'ds18', 'gameOfLife'
 
 
 def pinmap():
@@ -204,11 +238,13 @@ def pinmap():
     from LM_neopixel import pinmap as n_pmp
     pmp = o_pmp()
     pmp.update(t_pmp())
-    pmp.update(n_pmp())
+    pmp.update(pinmap_dump(['neop']))
     return pmp
 
 
 def help():
-    return 'load_n_init width=64 height=32 bootmsg="micrOS"', 'temperature', 'display period>=1000', 'press_event',\
-           'neopixel r=<0-255> g=<0-255> b=<0-255> br=<0-100> onoff=<toggle/on/off> smooth=<True/False>',\
-           'pinmap', 'lmdep'
+    return ('load_n_init width=64 height=32 bootmsg="micrOS"',
+            'temperature', 'display period>=1000',
+            'press_event',
+            'neopixel_color_wheel br=<0-100>',
+            'pinmap', 'lmdep')

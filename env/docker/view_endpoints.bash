46e27cb73923eb0581b06fd6ad7a51330ce86fee
BNM <ban.marcell@gmail.com>
2024-09-23 23:51:50 +0200

    docker-compose.yaml update

---
+++ env/docker/view_endpoints.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+url="http://localhost:3000"
+echo -e "OPEN GRAFANA: ${url}"
+open $url
+
+url="http://localhost:9090"
+echo -e "OPEN PROMETHEUS: ${url}"
+open $url
+
+url="http://localhost:5000"
+echo -e "OPEN GATEWAY: ${url}"
+open $url

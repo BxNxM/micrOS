12e8466411dae251c03c7403e370519f64f57a3d
BNM <ban.marcell@gmail.com>
2025-02-09 11:12:18 +0100

    [micrOS][PYPI] New Features: [Async Shell, Async Telegram bot (Notifications get), Web optimization + encoding fixes, pacman aka package manager (uos, mip): download (with /lib dir), rm, ls, dirtree, /logs dir handling in Logger.py (sys logs + dat logging)] + SIMulator (mode) adaptation to micropython uos + Minor fixes: lmpacman -> pacman with precompilation + new PYPI package - version: 2.9.7-0

--- micrOS/source/Shell.py
+++ micrOS/source/Shell.py
@@ -25,7 +25,7 @@ from Debug import errlog_add
 
 class Shell:
     __slots__ = ['__devfid', '__auth_mode', '__hwuid', '__auth_ok', '__conf_mode']
-    MICROS_VERSION = '2.9.6-0'
+    MICROS_VERSION = '2.9.7-0'
 
     def __init__(self):
         """
